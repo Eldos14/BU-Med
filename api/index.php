45ecc090f9f2b49d32c77a9d840073221413e3d6
aaa <aaa@mail.ru>
2026-06-26 17:14:55 +0500

    Fix: Add auto composer install in api bridge

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
-// Перенаправляем все запросы Vercel в главный файл Laravel
+// Автоматически устанавливаем composer зависимости, если их нет на сервере Vercel
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    exec('cd .. && curl -sS https://getcomposer.org | php && php composer.phar install --no-dev');
+}
+
+// Запускаем Laravel
 require __DIR__ . '/../public/index.php';
